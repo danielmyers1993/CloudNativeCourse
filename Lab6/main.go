f4825f4836738926ef6f37e3972187bb4e47660f
danielm <danielm@Ubuntu22.04.myguest.virtualbox.org>
2024-03-03 14:24:17 -0500

    Update main.go file

--- Lab6/main.go
+++ Lab6/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"gitlab.com/arunravindran/cloudnativecourse/lab6-external-rest-api/weather"
+	"github.com/danielmyers1993/CloudNativeCourse/Lab6/weather"
 )
 
 func main() {
