87f7dd04327981ed7791561b2765ceeb5e5775fb
danielm <danielm@Ubuntu22.04.myguest.virtualbox.org>
2024-03-18 17:33:46 -0400

    Update webserver

--- Lab4/webserver.go
+++ Lab4/webserver.go
@@ -4,137 +4,141 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strconv" //imported to parse string value of price into a float32 value
-	"sync"    //provides mutual exlusion in order to sync across database
+	"strconv"
+	"sync"
 )
 
 func main() {
-	db := database{"shoes": 50, "socks": 5}
-	http.HandleFunc("/list", db.list) //handlers for list, price, create, update, delete so it can read address properly
-	http.HandleFunc("/price", db.price)
-	http.HandleFunc("/create", db.create)
-	http.HandleFunc("/update", db.update)
-	http.HandleFunc("/delete", db.delete)
-	log.Fatal(http.ListenAndServe("localhost:8000", nil)) //no argmuent needs to be passed so nil is passed
+	db := database{data: map[string]dollars{"shoes": 50, "socks": 5}}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/list", db.list)
+	mux.HandleFunc("/price", db.price)
+
+	// Additional Handlers
+	mux.HandleFunc("/create", db.create)
+	mux.HandleFunc("/read", db.read)
+	mux.HandleFunc("/update", db.update)
+	mux.HandleFunc("/delete", db.delete)
+
+	log.Fatal(http.ListenAndServe(":8000", mux))
 }
 
-type dollars float32 // Dollars will be float32 to accommodate 32 bit decimals
+type dollars float32
 
 func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
 
-type database map[string]dollars
+// Changed Database to Struct for Lock/Unlock
+type database struct {
+	data map[string]dollars
+	mu   sync.RWMutex
+}
 
-func (db database) list(w http.ResponseWriter, req *http.Request) {
-	for item, price := range db {
+func (db *database) list(w http.ResponseWriter, req *http.Request) {
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+
+	for item, price := range db.data {
 		fmt.Fprintf(w, "%s: %s\n", item, price)
 	}
 }
 
-func (db database) price(w http.ResponseWriter, req *http.Request) {
+func (db *database) price(w http.ResponseWriter, req *http.Request) {
+
 	item := req.URL.Query().Get("item")
-	price, ok := db[item]
+
+	db.mu.RLock()
+	price, ok := db.data[item]
+	db.mu.RUnlock()
+
 	if !ok {
 		w.WriteHeader(http.StatusNotFound) // 404
 		fmt.Fprintf(w, "no such item: %q\n", item)
-		return
 	}
+
 	fmt.Fprintf(w, "%s\n", price)
 }
 
-// Create
-func (db database) create(w http.ResponseWriter, req *http.Request) { //takes in 2 arguments W, type http.responsewriter, req type *httprequest
-	//w used as instance to construct http response, passed as parameter to http handler func so that func can write reponse back to client
-	//req passed as parameter to function so that it can access information about type of request made, use to give appropriate response
-	query := req.URL.Query()    //grab query parameters of url request
-	item := query.Get("item")   //capture item name
-	price := query.Get("price") //get price value
+func (db *database) create(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	newPrice := req.URL.Query().Get("price")
 
-	// Check if item and price are present in the request
-	if item == "" || price == "" { //if item and price are empty
-		w.WriteHeader(http.StatusNotFound)          //updates status of error by writing price not found in w ResponseWriter
-		fmt.Fprintf(w, "item or price not found\n") //prints error msg
-		return
-	}
-
-	// Convert the price string to float32
-	p, err := strconv.ParseFloat(price, 32) //convert price string to float32 value
-	if err != nil {                         //if there is an error/fails to convert (as indicated by err does not equal to nil)
-		w.WriteHeader(http.StatusNotFound) //updates w ResponseWriter with invalid price status
-		fmt.Fprintf(w, "invalid price\n")
-		return
-	}
-
-	// Use a Mutex to synchronize access to the db
-	var mutex = &sync.Mutex{} //new mutex object to sync database
-	mutex.Lock()              //lock database before updating
-	db[item] = dollars(p)     //update database(adds new item with price)
-	mutex.Unlock()            //unlock after update
-
-	fmt.Fprintf(w, "item %s with price %s successfully created\n", item, price) //print successful message
-}
-
-// Update
-func (db database) update(w http.ResponseWriter, req *http.Request) {
-	query := req.URL.Query()    //grab query parameters of url request
-	item := query.Get("item")   //get item name
-	price := query.Get("price") //get item price
-
-	// Check if item and price are present in the request
-	if item == "" || price == "" {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "item or price not found\n")
-		return
-	}
-
-	// Convert the price string to float32
-	p, err := strconv.ParseFloat(price, 32)
+	price, err := strconv.ParseFloat(newPrice, 32)
+	// Handle Parsing Failure
 	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "invalid price\n")
+		w.WriteHeader(http.StatusBadRequest) // 400
+		fmt.Fprintf(w, "invalid price: %q\n", newPrice)
 		return
 	}
 
-	// Check if the item exists in the db
-	_, ok := db[item] //
-	if !ok {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "no such item: %q\n", item)
+	// Lock for Reading
+	db.mu.RLock()
+	_, ok := db.data[item]
+	db.mu.RUnlock()
+
+	if ok {
+		w.WriteHeader(http.StatusBadRequest) // 400
+		fmt.Fprintf(w, "item already exists: %s\n", item)
 		return
 	}
 
-	// Use a Mutex to synchronize access to the db
-	var mutex = &sync.Mutex{}
-	mutex.Lock()
-	db[item] = dollars(p)
-	mutex.Unlock()
+	db.mu.Lock()
+	(db.data)[item] = dollars(price)
+	db.mu.Unlock()
 
-	fmt.Fprintf(w, "item %s with price %s successfully updated\n", item, price)
+	fmt.Fprintf(w, "create item: %s, price: %s\n", item, (db.data)[item])
 }
 
-// Delete
-func (db database) delete(w http.ResponseWriter, req *http.Request) {
-	item := req.URL.Query().Get("item") //get url
+func (db *database) read(w http.ResponseWriter, req *http.Request) {
+	db.list(w, req)
+}
 
-	// Check if item is present in the request
-	if item == "" { //if item field is empty, update w responsewriter with error
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "item not found\n")
+func (db *database) update(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	newPrice := req.URL.Query().Get("price")
+
+	price, err := strconv.ParseFloat(newPrice, 32)
+	// Handle Parsing Failure
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest) // 400
+		fmt.Fprintf(w, "invalid price: %q\n", newPrice)
 		return
 	}
 
-	// Check if the item exists in the db
-	_, ok := db[item] //use item as key to look for in map, ok indicates whether or not item is in there
-	if !ok {          //if it is not in there/key is false
-		w.WriteHeader(http.StatusNotFound)         //display error
-		fmt.Fprintf(w, "no such item: %q\n", item) // writes err msg to response writer
+	// Lock for Reading
+	db.mu.RLock()
+	_, ok := db.data[item]
+	db.mu.RUnlock()
+
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest) // 400
+		fmt.Fprintf(w, "item does not exist: %s\n", item)
 		return
 	}
 
-	// Use a Mutex to synchronize access to the db
-	var mutex = &sync.Mutex{} //new instance of mutex for sync
-	mutex.Lock()              //locks db
-	delete(db, item)          //update db
-	mutex.Unlock()            //unlock db again
+	db.mu.Lock()
+	db.data[item] = dollars(price)
+	db.mu.Unlock()
 
-	fmt.Fprintf(w, "item %s successfully deleted\n", item) //print successfull message
+	fmt.Fprintf(w, "update item: %s, price: %s\n", item, db.data[item])
+}
+
+func (db *database) delete(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+
+	// Lock and Key for Synchronization
+	db.mu.RLock()
+	_, ok := db.data[item]
+	db.mu.RUnlock()
+
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest) // 400
+		fmt.Fprintf(w, "item does not exist: %s\n", item)
+		return
+	}
+
+	db.mu.Lock()
+	delete(db.data, item)
+	db.mu.Unlock()
+
+	fmt.Fprintf(w, "delete item: %s\n", item)
 }
